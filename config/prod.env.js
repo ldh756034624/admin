839375c81b3d38baf4f607a510fe2100d2e82a44
wudong <wudong@hey900.com>
2017-11-13 16:11:09 +0800

    环境

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://console-dev-h9.thy360.com/h9/admin/"',
-  IMGUP_API: '"https://console-dev-h9.thy360.com/h9/admin/common/file/upload"'
+  BASE_API: '"https://console-test-h9.thy360.com/h9/admin/"',
+  IMGUP_API: '"https://console-test-h9.thy360.com/h9/admin/common/file/upload"'
 }
