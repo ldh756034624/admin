7d6cf7f4086402a6ae44c1aced0caea9e04540c4
wudong <wudong@hey900.com>
2017-11-13 15:44:37 +0800

    环境配置

--- config/test.env.js
+++ config/test.env.js
@@ -3,5 +3,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"production"',
-  BASE_API: '"http://dev-gofar.jihe365.cn"'
+  BASE_API: '"https://console-test-h9.thy360.com/h9/admin/"',
+  IMGUP_API: '"https://console-test-h9.thy360.com/h9/admin/common/file/upload"'
 })
