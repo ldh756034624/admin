6502f25762cce5b1bd45eaf87115b7446784c6c1
wudong <wudong@hey900.com>
2018-01-18 10:09:47 +0800

    测试环境更改

--- config/dev.env.js
+++ config/dev.env.js
@@ -11,6 +11,6 @@ module.exports = merge(prodEnv, {
 // 本地调试release用
 // module.exports = merge(prodEnv, {
 //   NODE_ENV: '"development"',
-//   BASE_API: '"https://console-test-h9.thy360.com/h9/admin/"',
-//   IMGUP_API: '"https://console-test-h9.thy360.com/h9/admin/common/file/upload"'
+//   BASE_API: '"https://console-testnew-h9.thy360.com/h9/admin/"',
+//   IMGUP_API: '"https://console-testnew-h9.thy360.com/h9/admin/common/file/upload"'
 // })
