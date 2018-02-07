16dd02664879a98c3e9b2b9255a6aa61559125ba
喵呜 <meow.zhong@hey900.com>
2018-02-07 14:19:34 +0800

    1

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://console-dev-h9.thy360.com/h9/admin/"',
-  IMGUP_API: '"https://console-dev-h9.thy360.com/h9/admin/common/images/upload"',
-  FILE_API: '"https://console-dev-h9.thy360.com/h9/admin/common/file/upload"'
+  BASE_API: '"https://console-testnew-h9.thy360.com/h9/admin/"',
+  IMGUP_API: '"https://console-testnew-h9.thy360.com/h9/admin/common/images/upload"',
+  FILE_API: '"https://console-testnew-h9.thy360.com/h9/admin/common/file/upload"'
 }
